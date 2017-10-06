57de1b00f0f7f7ccbb52c010c91dfe7d82e23210
GuiguiCucu <guillaume.cutrone@gmail.com>
2017-10-06 14:26:03 +0200

    Update styledown.js

--- tasks/styledown.js
+++ tasks/styledown.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-var Styledown = require('lhs-styledown');
+var Styledown = require('@lahautesociete/styledown');
 var chalk = require('chalk');
 var extend = require('extend');
 var util = require('util');
